5f57fc2187b38eed104c3506413eaba04dafcbb1
Devan Farrell <devan.farrell16@gmail.com>
2020-03-09 23:17:15 -0700

    Removed unmaintained example code

--- example/src/redux/nestedCount.ts
+++
@@ -1,6 +0,0 @@
-import { createSlice } from 'redux-dogma';
-import { countSlice } from './count';
-import { countSlice2 } from './count2';
-
-export const countSliceParent = createSlice('countParent', {});
-countSliceParent.addSlice(countSlice).addSlice(countSlice2);
--- example/src/redux/sharedActions.ts
+++
@@ -1,3 +0,0 @@
-import { createAction } from 'redux-dogma';
-
-export const [CLEAR_INCREMENTS, clearIncrements] = createAction('CLEAR_INCREMENTS')
