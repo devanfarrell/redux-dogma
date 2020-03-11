a7b1435b986949448b5d4839d4879d02e65a0467
Devan Farrell <devan.farrell16@gmail.com>
2020-03-10 22:17:28 -0700

    Fixed indentation and immer types

--- src/sliceManager.ts
+++ src/sliceManager.ts
@@ -2,32 +2,32 @@ import { AnyAction, combineReducers, Reducer, ReducersMapObject } from 'redux';
 import { Slice, SliceManagerInterface } from './types';
 
 export default class SliceManager implements SliceManagerInterface {
-  combinedReducer: Reducer;
-  reducers: ReducersMapObject;
-  slices: Array<Slice<unknown>>;
+	combinedReducer: Reducer;
+	reducers: ReducersMapObject;
+	slices: Array<Slice<unknown>>;
 
-  constructor() {
-    this.reducers = {};
-    this.combinedReducer = combineReducers(this.reducers);
-    this.slices = [];
-    this.reduce = this.reduce.bind(this);
-    this.rootSaga = this.rootSaga.bind(this);
-  }
+	constructor() {
+		this.reducers = {};
+		this.combinedReducer = combineReducers(this.reducers);
+		this.slices = [];
+		this.reduce = this.reduce.bind(this);
+		this.rootSaga = this.rootSaga.bind(this);
+	}
 
-  public addSlice(slice: Slice<unknown>): void {
-    slice.resolveSlice([]);
-    this.slices.push(slice);
-    this.reducers[slice.key] = slice.reduce;
-    this.combinedReducer = combineReducers(this.reducers);
-  }
+	public addSlice(slice: Slice<unknown>): void {
+		slice.resolveSlice([]);
+		this.slices.push(slice);
+		this.reducers[slice.key] = slice.reduce;
+		this.combinedReducer = combineReducers(this.reducers);
+	}
 
-  public reduce(state: any, action: AnyAction) {
-    return this.combinedReducer(state, action);
-  }
+	public reduce(state: any, action: AnyAction) {
+		return this.combinedReducer(state, action);
+	}
 
-  public *rootSaga(): IterableIterator<any> {
-    for (let slice of this.slices) {
-      yield* slice.handleSaga();
-    }
-  }
+	public *rootSaga(): IterableIterator<any> {
+		for (let slice of this.slices) {
+			yield* slice.handleSaga();
+		}
+	}
 }
