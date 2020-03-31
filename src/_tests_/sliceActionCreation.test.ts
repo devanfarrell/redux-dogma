52bebab1dc674c7282102300908a9689cd5cdef1
Devan Farrell <devan.farrell16@gmail.com>
2020-03-31 00:22:40 -0700

    Resynced packagejson

--- src/_tests_/sliceActionCreation.test.ts
+++ src/_tests_/sliceActionCreation.test.ts
@@ -14,14 +14,14 @@ type PayloadTest = { name: string };
 describe('Test creation of all slice actions', () => {
 	const actionTest1 = slice1.createAction<PayloadTest>(testString, () => {});
 	const actionTest2 = slice2_1.createSideEffect<PayloadTest>(testString, function* () {});
-	const debouncedActionTest = slice2_1.createDebouncedSideEffect<PayloadTest>('debounced', function* () {})
+	const debouncedActionTest = slice2_1.createDebouncedSideEffect<PayloadTest>('debounced', function* () {});
 	const actionTest2_1 = slice2_1.createAction<PayloadTest>(testString, () => {});
 	const simpleAction = slice2_1.createSimpleAction('simple', () => {});
 
 	it('slice.createAction', () => {
 		expect(actionTest1({ name: 'testy test' })).toEqual({ type: testString, keyChain: ['slice1'], payload: { name: 'testy test' } });
 		expect(actionTest2({ name: 'testy test' })).toEqual({ type: `slice2/slice2_1/${testString}`, payload: { name: 'testy test' } });
-		expect(debouncedActionTest({ name: 'testy test' })).toEqual({ type: `slice2/slice2_1/debounced`, payload: { name: 'testy test' } })
+		expect(debouncedActionTest({ name: 'testy test' })).toEqual({ type: `slice2/slice2_1/debounced`, payload: { name: 'testy test' } });
 		expect(actionTest2_1({ name: 'testy test' })).toEqual({
 			type: testString,
 			keyChain: ['slice2', 'slice2_1'],
